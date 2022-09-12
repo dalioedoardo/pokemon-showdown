05f67ea2dc4b85ae96d5b736893db9b5a1b55c3f
dalioedoardo <45639023+dalioedoardo@users.noreply.github.com>
2022-09-12 16:38:53 +0200

    aggiunta 2.6

--- config/formats.ts
+++ config/formats.ts
@@ -170,6 +170,19 @@ export const Formats: FormatList = [
 		ruleset: ['Dynamax Clause', 'OHKO Clause', "Ambusher's Playground", 'Wrath of the Forest'],
 		banlist: ['all items', 'Endeavor', 'Destiny Bond', 'Counter', 'Mirror Coat', 'Metal Burst', 'Super Fang', "Nature's Madness"],
 	},
+	{
+		name: "2.6 GRASS TYPE BOSS CHALLENGE",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666135/">National Dex Metagame Discussion</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3667921/">National Dex Sample Teams</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666572/">National Dex Viability Rankings</a>`,
+		],
+
+		mod: 'gen8',
+		gameType: 'triples',
+		ruleset: ['Dynamax Clause', 'OHKO Clause', 'Boss Challenge', 'Voracious Aura', 'Rising Energy', 'Wrath of the Forest'],
+		banlist: ['Endeavor', 'Destiny Bond', 'Counter', 'Mirror Coat', 'Metal Burst', 'Super Fang', "Nature's Madness"],
+	},
 	
 	
 	
