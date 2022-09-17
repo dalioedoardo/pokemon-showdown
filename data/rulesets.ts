be1ebdf3f5ed5f247fbd3e21f0e9b51dd7745301
dalioedoardo <45639023+dalioedoardo@users.noreply.github.com>
2022-09-17 16:13:10 +0200

    aggiunta hint quantum festival

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -1018,6 +1018,7 @@ export const Rulesets: {[k: string]: FormatData} = {
 		onSwitchIn(pokemon){
 			if(!pokemon.getTypes.includes('Ghost')){
 				pokemon.setStatus('quantumstate',pokemon);
+				this.hint("This pokemon can be affected by multiple statuses!");
 			}
 			
 			if(this.turn<4 && pokemon.getTypes.includes('Ghost')){
