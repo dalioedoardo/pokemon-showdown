5ebce3a596a0e728d268b7713fa1df0fb781622f
dalioedoardo <45639023+dalioedoardo@users.noreply.github.com>
2022-09-19 15:29:16 +0200

    aggiunta 5.5

--- config/formats.ts
+++ config/formats.ts
@@ -399,8 +399,18 @@ export const Formats: FormatList = [
 		banlist: ['all items', 'Endeavor', 'Destiny Bond', 'Counter', 'Mirror Coat', 'Metal Burst', 'Super Fang', "Nature's Madness"],
 	},
 	
-	
-	
+	{
+		name: "[Gen 8] 5.5 GHOST TYPE GYM CHALLENGE",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666135/">National Dex Metagame Discussion</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3667921/">National Dex Sample Teams</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666572/">National Dex Viability Rankings</a>`,
+		],
+
+		mod: 'gen8',
+		ruleset: ['Dynamax Clause', 'OHKO Clause', 'Requiem Labanderoule', 'Whim of the Sith Lord'],
+		banlist: ['all items', 'Endeavor', 'Destiny Bond', 'Counter', 'Mirror Coat', 'Metal Burst', 'Super Fang', "Nature's Madness"],
+	},
 	
 	
 	
