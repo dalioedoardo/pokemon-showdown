31c8f10de444faf7c40bc546b103e6dd1e306a47
dalioedoardo <45639023+dalioedoardo@users.noreply.github.com>
2022-10-29 20:57:35 +0200

    aggiunta la powerup battle

--- config/formats.ts
+++ config/formats.ts
@@ -19,6 +19,23 @@ The column value will be ignored for repeat sections.
 
 export const Formats: FormatList = [
 
+	// POWERUP BATTLE - Harzen 29/19/2022
+	{
+		section: "ROCK TYPE GYM CHALLENGES",
+	},
+	{
+		name: "[Gen 8] POWERUP BATTLE",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666135/">National Dex Metagame Discussion</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3667921/">National Dex Sample Teams</a>`,
+			`&bullet; <a href="https://www.smogon.com/forums/threads/3666572/">National Dex Viability Rankings</a>`,
+		],
+
+		mod: 'gen8',
+		ruleset: ['Dynamax Clause', 'OHKO Clause', 'Powerup Clause'],
+	},	
+	
+	
 	
 	
 	
